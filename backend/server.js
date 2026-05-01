4a1ad996c68e2e635f30f09c85cf88f725d4bd1f
techjay <jaynimbark5869@gmail.com>
2026-05-01 08:55:52 +0530

    backend change

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,12 @@ app.get("/api/contact-test", (req, res) => {
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    app.listen(5000, () => console.log('Server running on port 5000'));
+    const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch(err => console.log(err));
   })
   .catch(err => console.log(err));
